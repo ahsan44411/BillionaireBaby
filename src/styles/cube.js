21277294dae8ab2a92c4319e2ed44d26e64a0716
Ahsan Mukhtar <ahsan44411@gmail.com>
2022-03-05 05:43:13 +0500

    f

--- src/styles/cube.js
+++ src/styles/cube.js
@@ -3,26 +3,22 @@ const defaultPerspective = '-150px';
 // Track the mouse movemont
 let mouseX = 0;
 let mouseY = 0;
-console.log('hey motherfucker')
 let lastXDeg = 180;
 let lastYDeg = 180;
 // The speed of the cube following movement
-const speed = 0.1;
+const speed = 0.3;
 $('.cube-animation-section').ready(() => {
-    console.log('i ran')
     setInterval(rotateCube, 66)
 })
 $('.cube-animation-section').mousemove(updateMousePosition);
 
 // Follow mouse movement
 function updateMousePosition(e) {
-    console.log('brother I also ran')
     mouseX = e.pageX / getWidth();
     mouseY = e.pageY / getHeight();
 }
 
 function rotateCube() {
-    console.log('for shizal ma nizal, ma nigga')
     lastXDeg = lastXDeg + (getAngle(mouseX) - lastXDeg
     ) * speed;
     lastYDeg = lastYDeg + (getAngle(mouseY) - lastYDeg
