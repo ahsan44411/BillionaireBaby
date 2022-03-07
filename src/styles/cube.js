8f1410fc9f091ed7c9189975ffe4822412a6024c
Ahsan Mukhtar <ahsan44411@gmail.com>
2022-03-08 04:50:07 +0500

    f

--- src/styles/cube.js
+++ src/styles/cube.js
@@ -1,51 +1,48 @@
 // Global variable
-const defaultPerspective = '60px';
+const defaultPerspective = '-150px';
 // Track the mouse movemont
 let mouseX = 0;
 let mouseY = 0;
-let lastXDeg = 60;
-let lastYDeg = 60;
+let lastXDeg = 180;
+let lastYDeg = 180;
 // The speed of the cube following movement
-const speed = 0.8;
-$('.cube-animation-section').ready(() => {
-    setInterval(rotateCube, 66)
+const speed = 1;
+$(document).ready(()=>{
+  setInterval(rotateCube, 66)
 })
-$('.cube-animation-section').mousemove(updateMousePosition);
-
+$(document).mousemove(updateMousePosition);
 // Follow mouse movement
 function updateMousePosition(e) {
-    mouseX = e.pageX / getWidth();
-    mouseY = e.pageY / getHeight();
+  mouseX = e.pageX/getWidth();
+  mouseY = e.pageY/getHeight();
 }
-
 function rotateCube() {
-    lastXDeg = lastXDeg + (getAngle(mouseX) - lastXDeg) * speed;
-    lastYDeg = lastYDeg + (getAngle(mouseY) - lastYDeg) * speed;
+  lastXDeg = lastXDeg + (getAngle(mouseX) - lastXDeg
+) * speed;
+  lastYDeg = lastYDeg + (getAngle(mouseY) - lastYDeg
+) * speed;
     let newStyle = `translateZ(${defaultPerspective}) rotateY(${lastXDeg}deg) rotateX(${lastYDeg}deg)`
-    $('.cube-wrapper').css('transform', newStyle);
+  $('.cube').css('transform', newStyle);
 }
-
 // this function return the corresponding angle for an x value
 function getAngle(x) {
-    return 360 - 180 * x;
+  return 180 - 360 * x;
 }
-
 function getWidth() {
-    return Math.max(
-        document.body.scrollWidth,
-        document.documentElement.scrollWidth,
-        document.body.offsetWidth,
-        document.documentElement.offsetWidth,
-        document.documentElement.clientWidth
-    )
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  )
 }
-
 function getHeight() {
-    return Math.max(
-        document.body.scrollHeight,
-        document.documentElement.scrollHeight,
-        document.body.offsetHeight,
-        document.documentElement.offsetHeight,
-        document.documentElement.clientHeight
-    )
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  )
 }
